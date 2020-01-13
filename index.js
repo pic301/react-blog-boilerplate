c15a911d6428b31147d91abea3f8be0c2f7fada3
pic301 <ppic301@gmail.com>
2020-01-14 02:43:01 +0900

    Register Function using Postman
    and Set up

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require('./models/user');
+
 mongoose.connect("mongodb+srv://pic301:abcd1234@react-blog-boilerplate-tj7g4.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
 .then(() => console.log('DB Connected!'))
 .catch(err => {
 console.log(Error, err.message);
 });
   
+app.use(bodyParser.urlencoded({ extended : true}));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/', (req,res) =>{
-    res.send('hello world')
+app.post('/api/users/register', (req,res)=>{
+    const user = new User(req.body)
+    user.save((err, userData)=>{
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 
