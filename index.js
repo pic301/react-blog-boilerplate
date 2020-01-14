987e7ee42986c19714e00e7df76b11b141290232
pic301 <ppic301@gmail.com>
2020-01-14 17:46:38 +0900

    Login Function
    with JsonWebToken

--- index.js
+++ index.js
@@ -1,31 +1,62 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const { User } = require('./models/user');
-const config = require('./config/key')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./models/user");
+const config = require("./config/key");
 
-mongoose.connect(config.mongoURI, { useNewUrlParser: true })
-.then(() => console.log('DB Connected!'))
-.catch(err => {
-console.log(Error, err.message);
-});
-  
-app.use(bodyParser.urlencoded({ extended : true}));
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB Connected!"))
+  .catch(err => {
+    console.log(Error, err.message);
+  });
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.post('/api/users/register', (req,res)=>{
-    const user = new User(req.body)
-    user.save((err, doc)=>{
-        if(err) return res.json({ success: false, err})
-        return res.status(200).json({
-            success: true,
-            userData: doc
-        })
-    })
-})
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
 
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+      userData: doc
+    });
+  });
+});
+ 
+app.post("/api/user/login", (req, res) => {
+  // 이메일 찾기
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not fount"
+      });
+    // 패스워드 검증
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({
+          loginSuccess: false,
+          message: "wrong password"
+        });
+      }
+    });
+    // 토큰생성
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res
+        .cookie("x_auth", user.token)
+        .status(200)
+        .json({
+          loginSuccess: true
+        });
+    });
+  });
+});
 
 app.listen(5000);
--- models/user.js
+++ models/user.js
@@ -1,34 +1,75 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRound = 10;
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
-    name:{
-        type:String,
-        maxlength:50,
-    },
-    email:{
-        type:String,
-        trim:true,
-        unique:1,
-    },
-    password:{
-        type:String,
-        minlength:5,
-    },
-    lastname:{
-        type:String,
-        maxlength: 50,
-    },
-    role:{
-        type:Number,
-        default: 0,
-    },
-    token:{
-        type: String,
-    },
-    tokenExp:{
-        type: Number,
-    }
-})
-const User = mongoose.model('User', userSchema)
+  name: {
+    type: String,
+    maxlength: 50
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: 1
+  },
+  password: {
+    type: String,
+    minlength: 5
+  },
+  lastname: {
+    type: String,
+    maxlength: 50
+  },
+  role: {
+    type: Number,
+    default: 0
+  },
+  token: {
+    type: String
+  },
+  tokenExp: {
+    type: Number
+  }
+});
+userSchema.pre("save", function(next) {
+  var user = this;
 
-module.exports = { User }
+  if (user.isModified("password")) {
+    bcrypt.genSalt(saltRound, function(err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) return next(err);
+        user.password = hash
+        next() 
+
+      });
+    });
+  } else {
+    next();
+  }
+});
+
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    becrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) 
+            return cb(err);
+            cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(),'secret')
+    user.token = token
+    user.save(function(err, user){
+        if(err)
+        return cb(err)
+        cb(null, user); 
+    })
+}
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
